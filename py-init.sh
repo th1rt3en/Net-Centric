d08b0cd5bd20db52a09411a7931366b411b9803c
th1rt3en <37678705+th1rt3en@users.noreply.github.com>
2018-03-29 22:15:29 +0700

    Update py-init.sh

--- py-init.sh
+++ py-init.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
-sudo apt-get install tomcat8
+sudo apt-get install tomcat8 -y
 sudo service tomcat8 start
-sudo apt-get install openjdk-8-jdk-headless
-sudo apt-get install python2.7
+sudo apt-get install openjdk-8-jdk-headless -y
+sudo apt-get install python2.7 -y
 wget https://res.cloudinary.com/negarr/raw/upload/v1522320270/py-fact.war
 sudo mv py-fact.war /var/lib/tomcat8/webapps/
